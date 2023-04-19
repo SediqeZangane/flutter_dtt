7a1ee417d26ddf0f29e1fd42aeb3cae8e1e7b574
sedighe zanganeh <se.zanganeh@yahoo.com>
2023-04-19 22:20:55 +0200

    fix AboutScreen Issues

--- lib/information/presentation/information_screen.dart
+++ lib/information/presentation/information_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InformationScreen extends StatelessWidget {
   const InformationScreen({super.key});
@@ -25,7 +26,10 @@ class InformationScreen extends StatelessWidget {
                 Text(
                   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                  style: Theme.of(context).textTheme.bodySmall,
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleSmall!
+                      .copyWith(fontWeight: FontWeight.w300),
                 ),
               ],
             ),
@@ -49,7 +53,30 @@ class InformationScreen extends StatelessWidget {
                       fit: BoxFit.contain,
                       filterQuality: FilterQuality.high,
                     ),
-                    Container(),
+                    Container(padding: EdgeInsets.only(left: 24),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'by DTT',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleSmall!
+                                .copyWith(fontWeight: FontWeight.w400),
+                          ),
+                          GestureDetector(
+                            child: Text('d-tt.nl',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleSmall!
+                                    .copyWith(fontWeight: FontWeight.w400,color: Colors.cyan[600]),),
+                            onTap: () {
+                              launchUrl(Uri.parse('https://www.d-tt.nl/'));
+                            },
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 )
               ],
